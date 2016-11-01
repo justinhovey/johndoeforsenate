1391b5ac4c828e23b53af2ac77da71f362e62296
jonathan facio <jonathan.facio@my.simpson.edu>
2016-11-01 12:32:02 -0500

    changed html to php

--- Resources/navigation.php
+++ Resources/navigation.php
@@ -1,12 +1,12 @@
     <nav>
         <ul>
-            <li><a href="index.html">Home</a></li>
-            <li><a href="johnabout.html">About John</a></li>
-            <li><a href="issues.html">Issues</a></li>
-            <li><a href="events.html">Events</a></li>
-            <li><a href="volunteer.html">Volunteer</a></li>
-            <li><a href="pollinglocation.html">Polling</a></li>
-            <li><a href="signup.html">Sign Up</a></li>
-            <li><a href="login.html">Login</a></li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="johnabout.php">About John</a></li>
+            <li><a href="issues.php">Issues</a></li>
+            <li><a href="events.php">Events</a></li>
+            <li><a href="volunteer.php">Volunteer</a></li>
+            <li><a href="pollinglocation.php">Polling</a></li>
+            <li><a href="signup.php">Sign Up</a></li>
+            <li><a href="login.php">Login</a></li>
         </ul>
-    </nav>
+    </nphp
